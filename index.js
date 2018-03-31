71c38a40dd1036250756225fa5521dd7a6021173
Michael Crabtree <622michael@gmail.com>
2018-03-31 12:36:05 -0400

    added routing between modules

--- index.js
+++ index.js
@@ -13,62 +13,45 @@ app.use(
 app.use(bodyParser.json());
 
 
-function getBusData(callback) {
-	// The busdata request may respond with a blank JSON if no buses are running
-	// For testing, we can use the testdata
 
-	var testResponse = require("./testdata/busdata")
-	callback(testResponse, null)
-
-	// The real body fo this method
-
-    // var options = {
-    // method: 'GET',
-    // url: 'https://cors-anywhere.herokuapp.com/http://bus.lehigh.edu/scripts/busdata.php?format=json',
-    // qs: {format: 'json'},
-    // headers:
-    //     {
-    //         'Postman-Token': 'b54f8a34-41f2-44e9-9925-bc453957485a',
-    //         'Cache-Control': 'no-cache',
-    //         'Origin':''
-    //     }
-    // };
-    // request(options, function (error, response, body) {
-    //     if (error) throw new Error(error);
-    //     console.log(body);
-    //     res.send(body);
-    // });
-}
 
 function output(req, res) {
-    getBusData(function(busdata, error) {
-        var speech = busdata["1"]["stops"]["0"]
-        res.json({
-            fulfillment_text: speech
-        })
-    });
+
 
 }
 
 function departFrom(req, res) {
-    res.json({
-        fulfillment_text: "The location request was successful!"
-    })
+
 
 }
 
+var busModule = require("./bus")
+
+var modules = {
+    "BUS": busModule
+}
+
+function send404(res) {
+    res.status(404)
+    res.send("")
+}
+
 app.post("/", function(req, res) {
-    console.log(req.body)
     var action = req.body.queryResult.action
-    if (action == "Location") {
-        departFrom(req, res)
-    } else if (action == "Test") {
-        output(req, res)
+
+    if (action.length < 4) {
+        send404(res)
     } else {
-        res.json({
-            fulfillment_text: "That is not an action!"
-        })
+        var module = action.substring(0, 3);
+        var func = action.substring(3);
+
+        if (modules[module] && modules[module][func]) {
+            modules[module][func](req, res)
+        } else {
+            send404(res)
+        }
     }
+
 });
 
 var port = process.env.PORT || 3000
