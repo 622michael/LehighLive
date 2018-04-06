caba9b0c6a3d254b93deffd1fd2604b1b639190a
Charles Inwald <cci219@lehigh.edu>
2018-04-06 15:32:37 -0400

    Calendar request

--- evt.js
+++ evt.js
@@ -1,8 +1,35 @@
-const request = require('request');
-
 const EVT_FUNCTION_ACTION_NAME_TO_FUNCTION = {
     'today': (req, res) => {
         console.log("Event Today reached");
+        var request = require("request");
+
+        var options = {
+            method: 'GET',
+            url: 'https://clients6.google.com/calendar/v3/calendars/indark@lehigh.edu/events',
+            qs:
+                {
+                    calendarId: 'indark%40lehigh.edu',
+                    singleEvents: 'true',
+                    timeZone: 'America%2FNew_York',
+                    maxAttendees: '1',
+                    maxResults: '250',
+                    sanitizeHtml: 'true',
+                    timeMin: '2018-04-06T00%3A00%3A00-04%3A00',
+                    timeMax: '2018-05-15T00%3A00%3A00-04%3A00',
+                    key: 'AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs'
+                },
+            headers:
+                {
+                    'Cache-Control': 'no-cache'
+                }
+        };
+
+        request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+
+            console.log(body);
+        });
+
         res.json({
             fulfillment_text: 'The calendar request was successful!'
         });
