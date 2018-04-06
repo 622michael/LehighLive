5f552a258100dc89ef152a022a3ec090b6ce4432
Charles Inwald <cci219@lehigh.edu>
2018-04-06 14:40:51 -0400

    Adding evt js to correspond to the event intent

--- evt.js
+++ evt.js
@@ -2,6 +2,7 @@ const request = require('request');
 
 const EVT_FUNCTION_ACTION_NAME_TO_FUNCTION = {
     'today': (req, res) => {
+        console.log("Event Today reached");
         res.json({
             fulfillment_text: 'The calendar request was successful!'
         });
