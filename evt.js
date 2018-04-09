ea503c5e4182a9433ce9a6c008e48e74a4faf12a
Charles Inwald <cci219@lehigh.edu>
2018-04-09 10:53:43 -0400

    Calendar request

--- evt.js
+++ evt.js
@@ -3,22 +3,9 @@ const EVT_FUNCTION_ACTION_NAME_TO_FUNCTION = {
         console.log("Event Today reached");
         var unirest = require("unirest");
 
-        var req = unirest("GET", "https://clients6.google.com/calendar/v3/calendars/indark@lehigh.edu/events");
-
-        req.query({
-            "calendarId": "indark%40lehigh.edu",
-            "singleEvents": "true",
-            "timeZone": "America%2FNew_York",
-            "maxAttendees": "1",
-            "maxResults": "250",
-            "sanitizeHtml": "true",
-            "timeMin": "2018-04-06T00%3A00%3A00-04%3A00",
-            "timeMax": "2018-05-15T00%3A00%3A00-04%3A00",
-            "key": "AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs"
-        });
-
+        var req = unirest("GET", "https://clients6.google.com/calendar/v3/calendars/indark@lehigh.edu/events?calendarId=indark%40lehigh.edu&singleEvents=true&timeZone=America%2FNew_York&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=2018-04-06T00%3A00%3A00-04%3A00&timeMax=2018-05-15T00%3A00%3A00-04%3A00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs");
+        
         req.headers({
-            "Postman-Token": "6255c7a5-f1bc-4dad-b0fe-f6fa6db6a644",
             "Cache-Control": "no-cache"
         });
 
