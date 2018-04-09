51bef2869d57a2da1b9603cd95a28604c771acc4
Charles Inwald <cci219@lehigh.edu>
2018-04-09 17:22:13 -0400

    Automated deploy

--- evt.js
+++ evt.js
@@ -16,11 +16,13 @@ const EVT_FUNCTION_ACTION_NAME_TO_FUNCTION = {
             console.log(res.body);
             var events = new Array();
             console.log(moment(Date.now()));
+            threeDaysFromNow = moment(Date.now().add(3,'d'));
             for (var i =0; i < res.body.items.length;i++){
                 var dateTime = res.body.items[i].start.dateTime;
-                console.log('moment : ' + moment(dateTime).fromNow() + " " + moment(dateTime).isAfter(Date.now()));
+                console.log('moment : ' + moment(dateTime).fromNow() + " " + moment(dateTime).isAfter(Date.now()) + " " + moment(dateTime).isBefore(threeDaysFromNow));
                 if(moment(dateTime).isAfter(Date.now())) {
                     events[i] = {"dateTime": dateTime};
+
                 }
             }
             console.log("EVENTS ARRAY");
