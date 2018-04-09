8a8410809886f05a5743f373157b6c004079608f
Charles Inwald <cci219@lehigh.edu>
2018-04-09 14:20:33 -0400

    Calendar request

--- evt.js
+++ evt.js
@@ -4,7 +4,7 @@ const EVT_FUNCTION_ACTION_NAME_TO_FUNCTION = {
         var unirest = require("unirest");
 
         var req = unirest("GET", "https://clients6.google.com/calendar/v3/calendars/indark@lehigh.edu/events?calendarId=indark%40lehigh.edu&singleEvents=true&timeZone=America%2FNew_York&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=2018-04-06T00%3A00%3A00-04%3A00&timeMax=2018-05-15T00%3A00%3A00-04%3A00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs");
-        
+
         req.headers({
             "Cache-Control": "no-cache"
         });
@@ -12,6 +12,8 @@ const EVT_FUNCTION_ACTION_NAME_TO_FUNCTION = {
         req.end(function (res) {
             if (res.error) throw new Error(res.error);
             console.log(res.body);
+            var dateTime = res.body.items[0].start;
+            console.log('DATETIME: ' + dateTime);
         });
 
         res.json({
