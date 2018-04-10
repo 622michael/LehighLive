334f77e26f0ee157cf8d6a24ae046328620b97bd
Charles Inwald <cci219@lehigh.edu>
2018-04-09 20:50:15 -0400

    Automated deploy

--- evt.js
+++ evt.js
@@ -1,15 +1,15 @@
-
+var unirest = require("unirest");
 const EVT_FUNCTION_ACTION_NAME_TO_FUNCTION = {
     'today': (req, res) => {
         console.log("Event Today reached");
-        var unirest = require("unirest");
-        let threeDay = ["why","the fuck", "does this not work"];
-        var req = unirest("GET", "https://clients6.google.com/calendar/v3/calendars/indark@lehigh.edu/events?calendarId=indark%40lehigh.edu&singleEvents=true&timeZone=America%2FNew_York&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=2018-04-06T00%3A00%3A00-04%3A00&timeMax=2018-05-15T00%3A00%3A00-04%3A00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs");
 
-        req.headers({
+        let threeDay = ["why","the fuck", "does this not work"];
+        let unirestReq = unirest("GET", "https://clients6.google.com/calendar/v3/calendars/indark@lehigh.edu/events?calendarId=indark%40lehigh.edu&singleEvents=true&timeZone=America%2FNew_York&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=2018-04-06T00%3A00%3A00-04%3A00&timeMax=2018-05-15T00%3A00%3A00-04%3A00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs");
+
+        unirestReq.headers({
             "Cache-Control": "no-cache"
         });
-        req.end(function (result) {
+        unirestReq.end(function (result) {
             var moment = require("moment");
             if (result.error) throw new Error(result.error);
             console.log(result.body);
