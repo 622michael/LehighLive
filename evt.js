dcad95a32f22eaf6e461218467c379e9faeae785
Charles Inwald <cci219@lehigh.edu>
2018-04-09 20:55:24 -0400

    Automated deploy

--- evt.js
+++ evt.js
@@ -3,7 +3,7 @@ const EVT_FUNCTION_ACTION_NAME_TO_FUNCTION = {
     'today': (req, res) => {
         console.log("Event Today reached");
 
-        let threeDay = ["why","the fuck", "does this not work"];
+        //let threeDay = ["why","the fuck", "does this not work"];
         let unirestReq = unirest("GET", "https://clients6.google.com/calendar/v3/calendars/indark@lehigh.edu/events?calendarId=indark%40lehigh.edu&singleEvents=true&timeZone=America%2FNew_York&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=2018-04-06T00%3A00%3A00-04%3A00&timeMax=2018-05-15T00%3A00%3A00-04%3A00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs");
 
         unirestReq.headers({
@@ -14,7 +14,7 @@ const EVT_FUNCTION_ACTION_NAME_TO_FUNCTION = {
             if (result.error) throw new Error(result.error);
             console.log(result.body);
             var events = new Array();
-            threeDay = [];
+            let threeDay = [];
             console.log(moment(Date.now()));
             threeDaysFromNow = moment(Date.now()).add(4,'d');
             aWeekFromNow = moment(Date.now()).add(7,'d');
