8676967673a64f3b8c82dda37d0e7df5841d8cc5
Charles Inwald <cci219@lehigh.edu>
2018-04-12 16:58:57 -0400

    Automated deploy

--- evt.js
+++ evt.js
@@ -115,6 +115,7 @@ function handleRequest(menudate, meal) {
     unirest.get("http://mc.lehigh.edu/services/dining/resident/Rathbone/Week_14_Rathbone.xml").end( (function (res) {
         if (res.error) throw new Error(res.error);
         console.log(res.body);
+        fs.writeFile('/food.xml',res.body,null,null);
         var weeklyMenus = JSON.parse(res.body).VFPData.weeklymenu;
         var station = "";
         for (i in weeklyMenus) {
