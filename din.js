7e56b30fcf2dfc11be526c12b4cecad2da37ab72
Charles Inwald <cci219@lehigh.edu>
2018-04-12 10:10:14 -0400

    Automated deploy

--- din.js
+++ din.js
@@ -1,8 +1,20 @@
-const unirest = require("unirest");
 const moment = require("moment");
 const EVT_FUNCTION_ACTION_NAME_TO_FUNCTION = {
     'time': (req, res) => {
         console.log("Dining  reached");
+        var request = require("request");
+
+        var options = { method: 'GET',
+            url: 'http://mc.lehigh.edu/services/dining/diningjson.html',
+            headers:
+                { 'Postman-Token': '0941584f-e6b8-471e-b562-bd3f487a71a9',
+                    'Cache-Control': 'no-cache' } };
+
+        request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+            console.log(body);
+        });
+
         res.json({
             fulfillment_text: "Dining reached"
         })
