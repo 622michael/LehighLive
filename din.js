e1e455921bfc9cf331ca9909c115c130ee70db52
Charles Inwald <cci219@lehigh.edu>
2018-04-12 09:47:59 -0400

    Automated deploy

---
+++ din.js
@@ -0,0 +1,12 @@
+const unirest = require("unirest");
+const moment = require("moment");
+const EVT_FUNCTION_ACTION_NAME_TO_FUNCTION = {
+    'today': (req, res) => {
+        console.log("Dining  reached");
+        res.json({
+            fulfillment_text: "Dining reached"
+        })
+    },
+};
+
+module.exports = EVT_FUNCTION_ACTION_NAME_TO_FUNCTION;
--- evt.js
+++ evt.js
@@ -3,9 +3,7 @@ const moment = require("moment");
 const EVT_FUNCTION_ACTION_NAME_TO_FUNCTION = {
     'today': (req, res) => {
         console.log("Event Today reached");
-
         let unirestReq = unirest("GET", "https://clients6.google.com/calendar/v3/calendars/indark@lehigh.edu/events?calendarId=indark%40lehigh.edu&singleEvents=true&timeZone=America%2FNew_York&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=2018-04-06T00%3A00%3A00-04%3A00&timeMax=2018-05-15T00%3A00%3A00-04%3A00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs");
-
         unirestReq.headers({
             "Cache-Control": "no-cache"
         });
