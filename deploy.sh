4e577435721070c7e6b57292a8d8834eef6d2a1d
Charles Inwald <cci219@lehigh.edu>
2018-04-12 10:10:58 -0400

    Automated deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "Automated deploy"
 git push heroku master
-curl 'https://api.dialogflow.com/v1/query?v=20170712&query=What%20events%20are%20going%20on%20today%3F&lang=en&sessionId=522aaa0e-3470-48e4-a9e8-421e717163d5&timezone=America/New_York' -H 'Authorization:Bearer d706f23e9add41f29ee093c9637491a8'
+curl -H "Content-Type: application/json; charset=utf-8"  -H "Authorization: Bearer ya29.c.ElqbBYFVKxQB8OtLD0rmMgZsmpPZaPa2-BQmabIc-7Vz7Hf_DXgTE6zMJ4_IR4R68sJOFTlImy5d90FIVnoom1rsm7F6WUW9Kkx_VP9mxVap7qHIKS7C0SnMjtE"  -d '{"queryInput":{"text":{"text":"When does lower close?","languageCode":"en"}},"queryParams":{"timeZone":"America/New_York"}}' "https://dialogflow.googleapis.com/v2beta1/projects/lehigh-8bfc4/agent/sessions/522aaa0e-3470-48e4-a9e8-421e717163d5:detectIntent"
