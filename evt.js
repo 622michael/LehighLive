4156537b49e48b1fc59a4f123daa1b15a311147b
Paul Grocholske <pag314@lehigh.edu>
2018-04-18 10:59:29 -0400

    Automated deploy

--- evt.js
+++ evt.js
@@ -97,9 +97,11 @@ const EVT_FUNCTION_ACTION_NAME_TO_FUNCTION = {
             }
             const jsonText = parser.toJson(data);
             const games = JSON.parse(jsonText)['scores']['game'];
+            const currentTime = moment();
+            console.log('Current time: ' + currentTime + '\n');
             const gameString = games.reduce((gameString, currentGame) => {
-                var currentTime = moment();
                 var gameTime = moment(currentGame['time'], 'MM-DD-YYYY hh:mm:ss A')
+                console.log('Game time: ' + gameTime + '\n');
                 if(gameTime.isBetween(currentTime, currentTime.add(3, 'd'))) {
                     const listItem = '- ' + currentGame['sport_abbrev'] + '\n';
                     return gameString + listItem;
