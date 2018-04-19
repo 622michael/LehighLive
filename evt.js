0742016079a035ad8c239485333e91def99f33fe
Charles Inwald <cci219@lehigh.edu>
2018-04-19 19:42:47 -0400

    fixing events carousel

--- evt.js
+++ evt.js
@@ -35,6 +35,9 @@ const parser = require('xml2json');
 
 const EVT_FUNCTION_ACTION_NAME_TO_FUNCTION = {
     'today': (req, res) => {
+      const queryResult = req.body.queryResult;
+      const date = queryResult.parameters;
+      console.log(date);
       console.log('Event Today reached');
       let unirestReq = unirest('GET', 'https://clients6.google.com/calendar/v3/calendars/indark@lehigh.edu/events?calendarId=indark%40lehigh.edu&singleEvents=true&timeZone=America%2FNew_York&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=2018-04-06T00%3A00%3A00-04%3A00&timeMax=2018-05-15T00%3A00%3A00-04%3A00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs');
       unirestReq.headers({
