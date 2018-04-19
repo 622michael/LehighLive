a57a8a730296b62b0b65180971dadc23e99012bf
Paul Grocholske <pag314@lehigh.edu>
2018-04-19 05:44:17 -0400

    Automated deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "Automated deploy"
-git push master && git push heroku master
+git push heroku master
 curl -H "Content-Type: application/json; charset=utf-8"  -H "Authorization: Bearer ya29.c.ElqfBdxRjdm43ldzPZVHKlX1YGzFSMpAyX9HVg25iRGdoKgYRISYBdG5vsem3zShHKpQXdcGMQYzUOM31AuYNqZavis79A5FlGlU-1yfmeivhMfUwzdozmYsY9o"  -d '{"queryInput":{"text":{"text":"What sports games are going on?","languageCode":"en"}},"queryParams":{"source":"test_console","timeZone":"America/New_York"}}' "https://dialogflow.googleapis.com/v2beta1/projects/lehigh-8bfc4/agent/sessions/f1e257d9-e8dd-452a-bef2-aec98c902dc3:detectIntent"
