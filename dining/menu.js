eebe29ec0085a9f120b5e708fca21b9287d40a48
Matt Herwig <herwig.matt@gmail.com>
2018-04-19 01:23:38 -0400

    Automated deploy

--- dining/menu.js
+++ dining/menu.js
@@ -110,7 +110,7 @@ const EVT_FUNCTION_ACTION_NAME_TO_FUNCTION = {
                             "title": `${meal}: ${location} Stations`,
                             "subtitle": "Lehigh University",
                             // "formattedText": "Choose a station.",
-                            "image": { "imageUri": "http://www.sse-llc.com/uploads/7/7/2/6/77268303/published/lehigh-university-rathbone-hall-2.jpg?1519764495"},
+                            "imageUri": "http://www.sse-llc.com/uploads/7/7/2/6/77268303/published/lehigh-university-rathbone-hall-2.jpg?1519764495",
                             "buttons": itemList
                         }
                 }
